5bdf705f06c5d6a7cd85e43d10a5fb0079d702dd
modpotato <81768237+modpotato@users.noreply.github.com>
2026-02-08 14:16:45 -0500

    fix: rm more privacy since no cloud

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,8 +1,6 @@
 
 export const GITHUB_URL = "https://github.com/potatocord/potatocord";
 export const SOURCE_CODE = "https://github.com/potatocord/vencord.dev";
-export const VENCORD_CLOUD_SOURCE_CODE_URL =
-    "https://github.com/potatocord/Vencloud";
 export const SPONSOR_URL = "https://github.com/sponsors/potatocord";
 
 export const DOWNLOAD_BASE =
@@ -12,13 +10,6 @@ export const PLUGINS_JSON_URL =
     "https://raw.githubusercontent.com/potatocord/builds/main/plugins.json";
 export const PLUGIN_READMES_JSON_URL =
     "https://raw.githubusercontent.com/potatocord/builds/main/plugin-readmes.json";
-
-export const FIREFOX_WEBSTORE_URL =
-    "https://addons.mozilla.org/en-GB/firefox/addon/vencord-web";
-export const CHROME_WEBSTORE_URL =
-    "https://chrome.google.com/webstore/detail/vencord-web/cbghhgpcnddeihccjmnadmkaejncjndb";
-export const USERSCRIPT_URL =
-    "https://raw.githubusercontent.com/potatocord/builds/main/Potatocord.user.js";
 
 export const IS_SERVER = import.meta.env.SSR;
 
